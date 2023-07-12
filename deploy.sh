8ae0e63f79ce63fa63f289321dfeeaa41d1c763e
🔥🚀wenjun.zhang <17621682391@163.com>
2023-07-12 17:22:27 +0800

    build
    build

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env sh
- 
+
 # 确保脚本抛出遇到的错误
 set -e
- 
-# 生成静态文件， npm run build
+
+# 生成静态文件
 npm run build
 
-# # 进入生成的文件夹
-cd ./public
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
-# git初始化，每次初始化不影响推送
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
-git add -A
-git commit -m 'deploy'
-git branch -M master
+git add .
+git commit -m 'first commit'
 
-# 如果你想要部署到 https://XXXX.github.io
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git remote add origin https://github.com/OMGRNGEDG/Photoshop.github.io.git
+git branch -M main
 git push -f https://github.com/OMGRNGEDG/Photoshop.github.io.git master
-echo "部署成功"
+
+cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,12 @@
 module.exports = {
   title: '个人博客',
   description: '分享笔记，技术博客',
+  base: '/<REPO>/',
   themeConfig: {
-      nav: [
-          { text: 'Home', link: '/' }
-      ],
-      sidebar: [
-          { title: 'Foo', path: '/foo' },
-          { title: 'Bar', path: '/bar' }
-      ]
-  }
-}
+    nav: [{ text: 'Home', link: '/' }],
+    sidebar: [
+      { title: 'Foo', path: '/foo' },
+      { title: 'Bar', path: '/bar' },
+    ],
+  },
+};
