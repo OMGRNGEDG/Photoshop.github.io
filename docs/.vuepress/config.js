747752934a64d049c0f2e31a7589d9639222a399
🔥🚀wenjun.zhang <17621682391@163.com>
2023-07-12 17:01:34 +0800

    初始化
    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '个人博客',
+  description: '分享笔记，技术博客',
+  themeConfig: {
+      nav: [
+          { text: 'Home', link: '/' }
+      ],
+      sidebar: [
+          { title: 'Foo', path: '/foo' },
+          { title: 'Bar', path: '/bar' }
+      ]
+  }
+}
