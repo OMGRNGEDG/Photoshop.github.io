e8a72564ccd731c77977eafb5714b7c0853318cb
🔥🚀wenjun.zhang <17621682391@163.com>
2023-07-14 12:37:06 +0800

    更新 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,9 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
 git push -f https://github.com/OMGRNGEDG/Photoshop.github.io.git gh-pages
-
 cd -
